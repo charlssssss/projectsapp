7083087e3dcb78dacad304341f11f79efd40968c
Charles John <charlescanete28@gmail.com>
2022-10-04 15:56:02 +0800

    Update layout.blade.php
    change theme

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Styles -->
-        <link href="https://bootswatch.com/5/flatly/bootstrap.min.css" rel="stylesheet">
+        <link href="https://bootswatch.com/5/lux/bootstrap.min.css" rel="stylesheet">
         <link href="/css/main.css" rel="stylesheet" type="text/css" >
         <!-- lux, flatly, litera -->
     </head>
